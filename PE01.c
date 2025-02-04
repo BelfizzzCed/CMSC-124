de374e6eb65373b6c2e2266c1c22a15918e8bd36
BelfizzzCed <cedricpsiosana@gmail.com>
2025-02-04 19:35:58 +0800

    Making the Dynamic Input
    Huwag i-delete, this is the function for inputting visually unlimited number of characters

--- PE01.c
+++ PE01.c
@@ -1,12 +1,35 @@
-// hello bading
-// okay na to??
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    printf("Hello bading\n");
-    printf("okay na to??\n");
-    return 0;
+char *dynamic_input(){
+    int size = 1;
+    char *buffer = (char *)malloc(size * sizeof(char));
+    if(!buffer) return NULL;
+    
+    char c;
+    int index = 0;
+    
+    while((c = getchar()) != '\n'){
+        buffer[index++] = c;
+        size++;
+        
+        buffer = (char *)realloc(buffer, size * sizeof(char));
+        if(!buffer) return NULL;
+    }
+    
+    buffer[index] = '\n';
+    return buffer;
 }
-// This is a push/pull request
+
+int main(){
+    
+    printf("Hello, please enter your string: ");
+    char *str = dynamic_input();
+    if(str){
+        printf("Your string is: %s", str);
+        free(str);
+    }
+    
+    return 0;    
+}
 
